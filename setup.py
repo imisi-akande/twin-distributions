52565c3f5236adbc940303f25696daff940b4819
Imisioluwa Akande <imisioluwa.akande@andela.com>
2020-05-27 18:24:42 +0100

    fix:update gitignore and setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='twin_distribution',
+      version='0.1',
+      description='Normal distributions',
+      packages=['twin_distribution'],
+      install_requires=['matplotlib'],
+      zip_safe=False)
