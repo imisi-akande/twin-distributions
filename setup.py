c7564860bd39a9eb6fbeb45e01e5ae9863b5ef89
Imisioluwa Akande <imisioluwa.akande@andela.com>
2020-05-27 19:27:45 +0100

    docs:modify-setup-description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='twin_distribution',
       version='0.1',
-      description='Normal distributions',
+      description='Normal and Binomial distributions',
       packages=['twin_distribution'],
       install_requires=['matplotlib'],
       zip_safe=False)
